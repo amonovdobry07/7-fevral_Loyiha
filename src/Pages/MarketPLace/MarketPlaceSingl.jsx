c6b49c4359bb0f28ea51450c1546ae24654fae53
Oga'bek Amonov <amonovdobry07@gmail.com>
2026-02-13 19:30:34 +0500

    market place link added

--- src/Pages/MarketPLace/MarketPlaceSingl.jsx
+++ src/Pages/MarketPLace/MarketPlaceSingl.jsx
@@ -2,10 +2,9 @@ import React, { useMemo } from "react";
 import { Link, useParams } from "react-router-dom";
 import "../../Styles/MarketPlaceSingl.scss";
 
-import p2 from "../../assets/images/ishi-1.jpg";
-import p3 from  "../../assets/images/ishi-2.jpg";
-import p4 from  "../../assets/images/ishi-3.jpg";
-
+import p2 from "../../assets/images/marketplace/image1.png";
+import p3 from "../../assets/images/marketplace/image.png";
+import p4 from   "../../assets/images/marketplace/image2.png";
 const MARKET_META = {
   wb: { title: "WILDBERRIES", accent: "#a24cff", platform: "https://www.wildberries.uz/" },
   ozon: { title: "OZON", accent: "#2b7cff", platform: "https://uz.ozon.com/" },
@@ -16,23 +15,23 @@ const MARKET_META = {
 const PRODUCTS_BY_MARKET = {
   ozon: [
     { id: "o2", title: "Devorga Naqshlar", img: p2, url: "https://uz.ozon.com/product/taburet-1-sht-3522427626/?oos_search=false" },
-    { id: "o3", title: "Yasama Uylar", img: p3, url: "https://uz.ozon.com/product/mangal-3522464747/?oos_search=false" },
-    { id: "o4", title: "Haykallar", img: p4, url: "https://uz.ozon.com/product/kuhonnyy-nozh-3522490666/?oos_search=false" },
+    { id: "o3", title: "Yasama Uylar", img: p3, url: "https://uz.ozon.com/product/statuetka-3530878850/?oos_search=false" },
+    { id: "o4", title: "Haykallar", img: p4, url: "https://uz.ozon.com/product/statuetka-3530873807/?abt_att=1&oos_search=false" },
   ],
   yandex: [
     { id: "y2", title: "Devorga Naqshlar", img: p2, url: "https://market.yandex.uz/card/sovremennyy-pristavnoy-stolik-v-stile-minimalizm-funktsionalnoye-i-stilnoye-resheniye-dlya-interyera-gostinoy-spalni/5129887823?businessId=216503443&showOriginalKmEmptyOffer=1&ogV=-12" },
-    { id: "y3", title: "Yasama Uylar", img: p3, url: "https://market.yandex.uz/card/mangal-metallicheskiy-5030-sm-praktichnoye-i-nadezhnoye-resheniye-dlya-prigotovleniya-shashlyka-myasa-ryby-i-ovoshchey-na-uglyakh/5130680248?businessId=216503443&showOriginalKmEmptyOffer=1&ogV=-12" },
-    { id: "y4", title: "Haykallar", img: p4, url: "https://market.yandex.uz/card/nabor-kukhonnykh-nozhey-iz-nerzhaveyushchey-stali-universalnoye-resheniye-dlya-domashney/5130721272?businessId=216503443&showOriginalKmEmptyOffer=1&ogV=-12" },
+    { id: "y3", title: "Yasama Uylar", img: p3, url: "https://market.yandex.uz/card/originalnaya-keramicheskaya-statuetka-s-natsionalnym-koloritom/5139491692?businessId=216503443&showOriginalKmEmptyOffer=1&ogV=-12" },
+    { id: "y4", title: "Haykallar", img: p4, url: "https://market.yandex.uz/card/originalnaya-keramicheskaya-statuetka-s-natsionalnym-koloritom-izobrazhayushchaya-pozhilogo-muzhchinu-v-traditsionnoy-odezhde/5139481577?businessId=216503443&showOriginalKmEmptyOffer=1&ogV=-12" },
   ],
   uzum: [
-    { id: "u2", title: "Devorga Naqshlar", img: p2, url: "https://uzum.uz/uz/product/zamonaviy-minimalist-yon-stolcha-2393189" },
+    { id: "u2", title: "Devorga Naqshlar", img: p2, url: "https://uzum.uz/uz/product/keramik-haykalcha-qol-mehnati-2405483?skuId=8637399" },
     { id: "u3", title: "Yasama Uylar", img: p3, url: "https://uzum.uz/uz/product/5030-sm-olchamdagi-metall-mangal-2393272" },
     { id: "u4", title: "Haykallar", img: p4, url: "https://uzum.uz/uz/product/zanglamaydigan-polatdan-tayyorlangan-2393410" },
   ],
   wb: [
     { id: "w2", title: "Devorga Naqshlar", img: p2, url: "https://www.wildberries.ru/catalog/817972141/detail.aspx?targetUrl=GP" },
-    { id: "w3", title: "Yasama Uylar", img: p3, url: "https://www.wildberries.ru/catalog/818034306/detail.aspx?targetUrl=GP" },
-    { id: "w4", title: "Haykallar", img: p4, url: "https://www.wildberries.ru/catalog/818068044/detail.aspx?targetUrl=GP" },
+    { id: "w3", title: "Yasama Uylar", img: p3, url: "https://www.wildberries.ru/catalog/824904665/detail.aspx?targetUrl=GP" },
+    { id: "w4", title: "Haykallar", img: p4, url: "https://www.wildberries.ru/catalog/824899878/detail.aspx?targetUrl=GP" },
   ],
 };
 
